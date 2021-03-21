6e0463f614e691e0345599ce5a0f1ba32c311b46
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-03-21 15:40:40 +0100

    Test throwTypeMismatchError

---
+++ spec/default/type-descriptor.spec.ts
@@ -0,0 +1,63 @@
+import test from 'ava';
+
+import {TypeDescriptor} from '../../src';
+import {getDiagnostic} from '../../src/diagnostics';
+
+export class ThrowTypeMismatchErrorSpec extends TypeDescriptor {
+    test(value: any) {
+        this.throwTypeMismatchError({
+            path: '',
+            expectedType: 'Something',
+            source: value,
+        });
+    }
+
+    fromJson(): any {
+        throw new Error('Not implemented');
+    }
+
+    toJson(): any {
+        throw new Error('Not implemented');
+    }
+
+    getFriendlyName(): string {
+        throw new Error('Not implemented');
+    }
+}
+const throwTypeMismatchErrorSpec = new ThrowTypeMismatchErrorSpec();
+
+test('throwTypeMismatchError should handle undefined source', t => {
+    t.throws(() => {
+        throwTypeMismatchErrorSpec.test(undefined);
+    }, {
+        message: getDiagnostic('invalidValueError', {
+            actualType: 'undefined',
+            expectedType: 'Something',
+            path: '',
+        }),
+    });
+});
+
+test('throwTypeMismatchError should handle null source', t => {
+    t.throws(() => {
+        throwTypeMismatchErrorSpec.test(null);
+    }, {
+        message: getDiagnostic('invalidValueError', {
+            actualType: 'undefined',
+            expectedType: 'Something',
+            path: '',
+        }),
+    });
+});
+
+test('throwTypeMismatchError should handle non-object source', t => {
+    t.throws(() => {
+        throwTypeMismatchErrorSpec.test('');
+    }, {
+        message: getDiagnostic('invalidValueError', {
+            actualType: 'String',
+            expectedType: 'Something',
+            path: '',
+        }),
+    });
+});
